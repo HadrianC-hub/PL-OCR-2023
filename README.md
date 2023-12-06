688d0c646684bd0c08b568c418c540a65741c012
Adrián Hernández <hadrianc02@gmail.com>
2023-12-06 18:37:15 -0500

    Desarrollo con PyTesseract

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # PL-OCR-2023
- Optical-Character-Recognition System:                                              MATCOM--2023--Adrián Hernández Castellanos--C211
+ Optical-Character-Recognition System:                                                  MATCOM--2023--Adrián Hernández Castellanos
  
- Este proyecto consiste en desarrollar un Sistema de Reconocimiento Óptico de Caracteres, al cual se le insertará una imagen o conjunto de imágenes y se generará el texto contenido en las imágenes a través de una interfaz gráfica. El proyecto está desarrollado en python.
+ Este repositorio contiene el código fuente de un Sistema de Reconocimiento Óptico de Caracteres, al cual se le insertará una imagen o conjunto de imágenes y se generará el texto contenido en las imágenes a través de una interfaz gráfica. El proyecto está desarrollado en el lenguaje de programación Python con las herramientas especificadas a continuación...
 
  Herramientas utilizadas en el desarrollo de este proyecto:
- -Python 3.12.0
- -OpenCV 4.8.1.78
- -Matplotlib 3.8.0
+ -Python 3.12.0         (Lenguaje de programación)
+ -OpenCV 4.8.1.78       (Procesaminto de imágenes)
+ -Matplotlib 3.8.0      (Operaciones de mapeo de imágenes)
+ -Pytesseract           (Modelo de aprendizaje preentrenado con herramientas de OCR)
 
- ACTUALMENTE EN DESARROLLO!!!
+ INSTRUCCIONES DE USO:
+ -> Al ser una versión portable, el OCR no necesita una instalación. El ejecutable se puede compilar a partir del archivo main.py
+ -> Antes de iniciar, debe copiar las imágenes que desea convertir a la carpeta _Content_
+ -> Al ejecutar el sistema se generará un archivo .txt en la carpeta Output con el nombre de la imágen y su conteido
+ -> Los formatos de imágen admitidos son: *.jpg y .png*
--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 #Importando librerías escenciales
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 import cv2 as cv
-
+import pytesseract
+from pytesseract import Output
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 #------------------------------------------MÉTODOS USADOS------------------------------------------
 
 def binarize (img):
     #Llevando a escala de grises
     gray_img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    #cv.imshow('Gray Img',gray_img)
-    #cv.waitKey(0)
 
     #Aplicando un filtro de mediana para eliminar el ruido
     denoised_img = cv.medianBlur(gray_img, 3)
-    #cv.imshow('Denoised Img',denoised_img)
-    #cv.waitKey(0)
 
     #Ecualizando imágen por celdas
     clahe=cv.createCLAHE(clipLimit=3.0,tileGridSize=(8,8)) #Instanciando algoritmo de ecualización
     equalized_img=clahe.apply(denoised_img)
-    #cv.imshow('Equalized Img',equalized_img)
-    #cv.waitKey(0)
 
     #Detectando los bordes de la imágen para corregir la inclinación
     edges = cv.Canny(equalized_img, 50, 150, apertureSize=3)
@@ -36,53 +33,67 @@ def binarize (img):
     rows, cols = equalized_img.shape[:2]
     M = cv.getRotationMatrix2D((cols/2, rows/2), angle, 1)
     rotated_img = cv.warpAffine(equalized_img, M, (cols, rows), flags=cv.INTER_CUBIC, borderMode=cv.BORDER_REPLICATE)
-    #cv.imshow('Rotated Img',rotated_img)
-    #cv.waitKey(0)
 
     #Extrayendo contornos de imágen aplicando la binarización de OTSU (Halla el valor óptimo del umbral en la imágen)
     _,thresh=cv.threshold(rotated_img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-    cv.imshow('Threshold Img',thresh)
-    cv.waitKey(0)
     return thresh
 
 def get_textblocks (binarized_img, img):
     #Encontrar los contornos en la imagen
-    cont_img = img
-    contours, _ = cv.findContours(binarized_img, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-    for i in range (len(contours)):
-        cv.drawContours(cont_img, contours, i, (0, 255, 0), 1)
-    cv.imshow('Contornos',cont_img)
-    cv.waitKey(0)
+    d = pytesseract.image_to_data(binarized_img, output_type=Output.DICT)
 
-    #Inicializar una lista para almacenar los bloques de texto encontrados
-    text_blocks = []
+    #Imprimir las coordenadas y el texto de cada bloque de texto
+    for i in range(len(d['text'])):
+        if int(d['conf'][i]) > 60:
+            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+            cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            text = d['text'][i]
+            cv.putText(img, text, (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-    #Recorrer todos los contornos encontrados
-    for contour in contours:
-        #Obtener el rectángulo delimitador del contorno
-        x, y, w, h = cv.boundingRect(contour)
-        if (w*h>50):
-            text_blocks.append((x, y, w, h))
-
-    #Mostrar los bloques de texto encontrados en la imagen original
-    image = img
-    for block in text_blocks:
-        x, y, w, h = block
-        cv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 255), 1)
-
-    cv.imshow('Text Blocks', image)
+    cv.imshow('img', img)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
 
 #------------------------------------------EJECUCIÓN PRINCIPAL DEL PROGRAMA------------------------------------------
 
-#Cargando imágen
-img=cv.imread('samples/sample.png')
+#Estableciendo carpeta de contenidos
+folder='Content' #Cambiar solo si sabe lo que hace
+out='Output'
+print('Carpeta predeterminada de entrada: '+folder+'\n')
+print('Carpeta predeterminada de salida: '+out+'\n')
 
-#Binarizando imágen
-binarized_img = binarize(img)
+#Limpiando directorio de salida
+out_f=os.listdir(out)
+for file in out_f:
+    os.remove(out+'/'+file)
+print('Directorio de salida limpiado con éxito'+'\n')
 
-#Encontrando bloques de texto
-get_textblocks(binarized_img, img)
+#Revisando directorio de contenido
+files = os.listdir(folder)
 
+#Filtramos los archivos .jpg y .png
+f_files=[]
+for file in files:
+    if(file.endswith('.jpg') or file.endswith('.png')):
+        f_files.append(file)
+
+#Iterando por cada archivo
+for file in files:
+    #Cargando imágen
+    img=cv.imread(folder+"/"+file)
+
+    #Binarizando imágen
+    binarized_img = binarize(img)
+
+    #Encontrando bloques de texto
+    #get_textblocks(binarized_img, img)
+
+    #Generando texto a partir de la imágen
+    text = pytesseract.image_to_string(binarized_img,lang='spa')
+    f=open(out+'/'+file+'.txt','x')
+    f.write(file+':\n'+text)
+    f.close
+    print('Analizado archivo '+file+': Creado informe en '+out+'\n')
+
+input('Ejecución finalizada, presione cualquier tecla para cerrar la consola...')
