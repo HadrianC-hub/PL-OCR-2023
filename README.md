1369b89b490d8a4726b106e85c27a7bc64f7f46a
Adrián Hernández <hadrianc02@gmail.com>
2023-12-14 01:22:15 -0500

    Final Release

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # PL-OCR-2023
- Optical-Character-Recognition System:                                                  MATCOM--2023--Adrián Hernández Castellanos
+ Optical-Character-Recognition System:--------MATCOM--2023--Adrián Hernández Castellanos
  
  Este repositorio contiene el código fuente de un Sistema de Reconocimiento Óptico de Caracteres, al cual se le insertará una imagen o conjunto de imágenes y se generará el texto contenido en las imágenes a través de una interfaz gráfica. El proyecto está desarrollado en el lenguaje de programación Python con las herramientas especificadas a continuación...
 
@@ -14,3 +14,13 @@
  -> Antes de iniciar, debe copiar las imágenes que desea convertir a la carpeta _Content_
  -> Al ejecutar el sistema se generará un archivo .txt en la carpeta Output con el nombre de la imágen y su conteido
  -> Los formatos de imágen admitidos son: *.jpg y .png*
+
+ BREVE EXPLICACIÓN DEL FUNCIONAMIENTO:
+ Este proyecto usa dos herramientas fundamentales para realizar su función.
+ OpenCV es una biblioteca de visión computacional con una extensa cantidad de métodos que ofrecen múltiples variantes de procesamiento de imágenes para desarrollar sistemas de interpretación, comprensión, búsqueda y predicción de datos relacionados o contenidos en una imágen.
+ El trabajo de esta biblioteca de clases en este sistema no es más que recibir una entrada y optimizarla según sus condiciones, aplicando los filtros adecuados para mejorar la calidad del análisis posterior.
+ Por otro lado, Tesseract es una biblioteca con un modelo preentrenado integrado, útil para encontrar cuadros de texto y caracteres en imágenes. El entrenamiento recibido previamente por este modelo ha sido mejorado a partir de redes neuronales conocidas como Long Short-Term Memory. También cuenta con una funcionalidad de post-procesado en la cual se corrigen las predicciones del sistema en comparación con las respuestas esperadas a partir de la información del dataset. Este modelo puede ser entrenado bajo un contexto específico para estimular y mejorar su efectividad en dicho espacio. Por ejemplo, usando texto manuscrito en un lenguaje específico.
+ El sistema consta de 3 partes de funcionamiento que se complementan con el preprocesamiento de la imagen:
+ --Detección de líneas de texto: La cual constituye la principal ejecución del programa, y se encarga de trazar una búsqueda de un fragmento rectangular de texto con irregularidades despreciables en los cuales su contenido contenga gran cantidad de bordes .
+ --Identificación de caracteres: Analizar la estructura de cada borde, realizar un mapeo y comparar con los caracteres del conjunto de datos para retornar una respuesta cercana a lo esperado.
+ --Estructuramiento del texto a devolver
--- main.py
+++ main.py
@@ -14,14 +14,19 @@ def binarize (img):
     #Llevando a escala de grises
     gray_img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
-    #Aplicando un filtro de mediana para eliminar el ruido
+    #Aplicando un filtro de mediana para minimizar el ruido (manchas, diferencias de iluminación, etc)
     denoised_img = cv.medianBlur(gray_img, 3)
 
     #Ecualizando imágen por celdas
+    #La ecualización es aplicada para garantizar que el ruido minimizado de la imágen desaparezca 
+    #casi por completo...
     clahe=cv.createCLAHE(clipLimit=3.0,tileGridSize=(8,8)) #Instanciando algoritmo de ecualización
     equalized_img=clahe.apply(denoised_img)
 
     #Detectando los bordes de la imágen para corregir la inclinación
+    #En el caso en que una imágen no esté en un ángulo de inclinación correcto, se corrige a partir de
+    #este método (su funcionamiento depende en gran medida de la evaluación de los ángulos del borde 
+    #exterior más destacado)
     edges = cv.Canny(equalized_img, 50, 150, apertureSize=3)
     lines = cv.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=100, maxLineGap=10)
 
@@ -36,13 +41,18 @@ def binarize (img):
 
     #Extrayendo contornos de imágen aplicando la binarización de OTSU (Halla el valor óptimo del umbral en la imágen)
     _,thresh=cv.threshold(rotated_img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    #En este punto obtuvimos la mejor calidad que pudimos extraer de la imagen, ahora convertida a una imagen binaria.
     return thresh
 
 def get_textblocks (binarized_img, img):
-    #Encontrar los contornos en la imagen
+    #Encontrar los contornos en la imagen (para poder distinguir cada cuadro)
     d = pytesseract.image_to_data(binarized_img, output_type=Output.DICT)
 
     #Imprimir las coordenadas y el texto de cada bloque de texto
+    #La funcionalidad de este algoritmo radica en delimitar un texto por 4 puntos y luego trazar una línea
+    #para conectarlos. Mientras más rectangular sea la figura obtenida, mejor definida estará la línea de texto
+    #De igual manera es capaz de delimitar dos bloques de texto contiguos debido al espacio que existe entre
+    #ellos y a la diferencia de tamaño de sus correspondientes rectángulos.
     for i in range(len(d['text'])):
         if int(d['conf'][i]) > 60:
             (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
@@ -88,12 +98,21 @@ for file in files:
 
     #Encontrando bloques de texto
     #get_textblocks(binarized_img, img)
+    #Este algoritmo simula el método de extracción de bloques de texto utilizado por Tesseract, fue la
+    #primera implementación de esta funcionalidad y es una alternativa válida que solamente utiliza OpenCV
 
     #Generando texto a partir de la imágen
     text = pytesseract.image_to_string(binarized_img,lang='spa')
+
+    #Esta línea está comentada porque representa el post-procesamiento de los datos
+    #data = data.replace('subcadena a reemplazar', 'subcadena de reemplazo')
+    #Este método reemplaza todas las ocurrencias de una subcadena específica en una cadena con otra subcadena.
+
+    #Creación de archivo de datos con el informe del texto extraído
     f=open(out+'/'+file+'.txt','x')
     f.write(file+':\n'+text)
     f.close
     print('Analizado archivo '+file+': Creado informe en '+out+'\n')
 
 input('Ejecución finalizada, presione cualquier tecla para cerrar la consola...')
+ 
